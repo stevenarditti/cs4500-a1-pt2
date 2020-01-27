6cfdf070abe8c17d74d2ec31f4cf7d00487011c6
Steven Arditti <arditti.s@husky.neu.edu>
2020-01-27 20:40:05 +0000

    dummy return values in object.h and string.h

--- object.h
+++ object.h
@@ -7,11 +7,11 @@ class Object {
     /**
      * Returns a nearly unique hash of this object 
     */ 
-    size_t hash() { }
+    size_t hash() { return 0; }
 
     /**
      * Checks if the given object is equal to this object
     */
-    virtual size_t equals(Object o) {  }
+    virtual size_t equals(Object o) { return 0; }
 
 };
--- string.h
+++ string.h
@@ -9,32 +9,32 @@ class String : public Object {
     /**
      * Redundent method for user friendliness.
      */ 
-    size_t size() { }
+    size_t size() { return 0; }
     
     /**
      * Alphanumerically ompares the given String to this String
      * Returns 0 if equal, 1 if if the given String is alphumerically higher,
      * or -1 if the given String is alphanumerically lower.
     */
-    int compare(String s) { }
+    int compare(String s) { return 0; }
 
     /**
      * Checks if the given object is equal to this object.
     */
-    size_t equals(Object o) { }
+    size_t equals(Object o) { return 0; }
 
     /**
      * Returns the String beginning at the starting index and
      * ending before the end index.
      * Returns null if either index is invalid.
      */
-    String* substring(size_t start, size_t end) { }
+    String* substring(size_t start, size_t end) { return nullptr; }
 
     /**
      * Concatenates the given String to this String.
      * Returns pointer to the resulting String.
      */
-    String* concat(String* s) { }
+    String* concat(String* s) { return nullptr; }
 
 
     /**
@@ -46,12 +46,12 @@ class String : public Object {
     /**
      * Returns a list of Strings split at the given separator.
      */
-    String** split(char* separator) {  }
+    String** split(char* separator) { return nullptr; }
 
     /**
      * Checks if this String contains the given String.
      */
-    bool contains(String* sub) { }
+    bool contains(String* sub) { return 0; }
 
 
     /**
